dff7f0f4fe2e7650bcfef5d7bf43b8c2127dc109
VinderX <kar.galiullin@gmail.com>
2026-04-21 19:03:30 +0300

    (manApi) basically done

---
+++ Broker/Management.Api/Connection/IBrokerConnection.cs
@@ -0,0 +1,9 @@
+﻿using Broker.Contracts;
+
+namespace Api.Connection;
+
+public interface IBrokerConnection : IDisposable
+{
+    MonitoringService.MonitoringServiceClient GetMonitoringClient();
+    ConfigService.ConfigServiceClient GetConfigClient();
+}
---
+++ Broker/Management.Api/MonitoringClient/MonitoringClient.cs
@@ -0,0 +1,24 @@
+﻿using Api.Connection;
+using Broker.Contracts;
+
+namespace Api.MonitoringClient;
+
+public class MonitoringClient(IBrokerConnection connection)
+{
+    private readonly MonitoringService.MonitoringServiceClient _client = connection.GetMonitoringClient();
+
+    public async Task<BrokerStatus> GetBrokerStatus(CancellationToken ct)
+    {
+        return await _client.GetBrokerStatusAsync(new GetBrokerStatusRequest { }, cancellationToken: ct);
+    }
+
+    public async Task<GetMetricsResponse> GetMetricsAsync(string queueName, long from, long to, CancellationToken ct)
+    {
+        return await _client.GetMetricsAsync(new GetMetricsRequest
+        {
+            QueueName = queueName,
+            FromTimestamp = from,
+            ToTimestamp = to,
+        }, cancellationToken: ct);
+    }
+}
